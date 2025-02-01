966e60a2dbab9c7d9ace4ae50ae36c00c21e66fe
nirmal523 <kadanirmalkumar@gmail.com>
2025-02-01 18:46:32 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,12 @@
+public class DataTypes {
+    public static void main(String[] args) {
+    int a=10;
+    String b="shiva";
+    char c='k';
+    double d=12.56;
+    System.out.println("this is an int dt and it variable is a and value is "+a);
+    System.out.println("this is an String dt and it variable is a and value is "+b);
+    System.out.println("this is an char dt and it variable is a and value is "+c);
+    System.out.println("this is an double dt and it variable is a and value is "+d);
+
+}}
